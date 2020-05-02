2c6d371502bec2dae34771b19f0752cac75718d9
Justin Wash <justin.wash@pm.me>
2020-05-01 19:13:08 -0500

    add try on /connect

--- server.ts
+++ server.ts
@@ -8,7 +8,11 @@ const qController = new QueueController();
 qController.startTimer();
 
 app.get('/connect', (req, res) => {
-  qController.connect(req, res);
+  try {
+    qController.connect(req, res);
+  } catch {
+    console.log('connect failed. bad request?');
+  }
 });
 
 app.listen(port, () => console.log(`Matchmaking server listening at http://localhost:${port}`));
