b1f641e662cca6647d4aa81b50748841a1119021
Justin Wash <justin.wash@pm.me>
2020-05-04 19:43:02 -0500

    rewrite http only

--- controllers/QueueController.ts
+++ controllers/QueueController.ts
@@ -1,10 +1,9 @@
 import Player from '../models/Player';
 import { v4 as uuid } from 'uuid';
-const WebSocket = require('ws');
 
 export default class QueueController {
-  players: any[] = [];
-  queue: any[] = [];
+  players: Player[] = [];
+  queue: Player[] = [];
 
   startTimer() {
     setInterval(() => {
@@ -13,81 +12,221 @@ export default class QueueController {
   }
 
   connect(req, res) {
+    var duplicate = this.players.find((p) => {
+      return (
+        (p.address == req.headers['x-forwarded-for'] || req.connection.remoteAddress) &&
+        p.lanAddress == req.query.lanAddress &&
+        p.serverPort == req.query.serverPort
+      );
+    });
+
+    if (duplicate) {
+      res.json({
+        message: `duplicate player: ${duplicate.id}`,
+        data: {
+          id: duplicate.id,
+        },
+      });
+      return;
+    }
+
     let player: Player = {
       id: uuid(),
       address: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
       lanAddress: req.query.lanAddress,
-      socketPort: req.query.socketPort,
       serverPort: req.query.serverPort,
-      ws: null,
+      gameId: req.query.gameId,
+      matchFound: null,
+      opponent: null,
       host: false,
+      timeout: null,
     };
 
-    let connection;
+    player.timeout = setTimeout(() => this.timeout(player), 300000); // disconnect if no activity for 5 minutes
 
-    if (player.address.includes('::')) {
-      try {
-        connection = new WebSocket(`ws://[${player.address}]:${player.socketPort}`);
-      } catch (err) {
-        console.log('error connecting to websocket server - ' + err + ': player ' + player.id);
-      }
-    } else {
-      try {
-        connection = new WebSocket(`ws://${player.address}:${player.socketPort}`);
-      } catch (err) {
-        console.log('error connecting to websocket server - ' + err + ': player ' + player.id);
-      }
-    }
-
-    connection.on('open', () => {
-      connection.send(
-        JSON.stringify({
-          type: 'confirmation',
-          data: `matchmaking server connected to websocket on ${player.socketPort}`,
-        })
-      );
-    });
-
-    connection.on('message', (message) => {
-      if (message.toString() == 'connected') {
-        console.log(`connection to player ${player.id} successful`);
-      }
-
-      if (message.toString() == 'start matching') {
-        var indexToAdd = this.queue.indexOf(player);
-        if (indexToAdd == -1) {
-          this.queue.push(player);
-          console.log(`player ${player.id} added to the queue`);
-        } else {
-          console.log(`player ${player.id} already in the queue`);
-        }
-      }
-
-      if (message.toString() == 'cancel matching') {
-        var indexToRemove = this.queue.indexOf(player);
-        if (indexToRemove != -1) {
-          this.queue.splice(indexToRemove);
-          console.log(`player ${player.id} removed from the queue`);
-        } else {
-          console.log(`player ${player.id} not in the queue`);
-        }
-      }
-    });
-
-    connection.on('close', () => {
-      var indexToRemove = this.players.indexOf(player);
-      if (indexToRemove != -1) {
-        this.players.splice(indexToRemove);
-        console.log(`player ${player.id} disconnected`);
-      } else {
-        console.log(`player ${player.id} not connected`);
-      }
-    });
-
-    player.ws = connection;
     this.players.push(player);
 
-    res.json(`server reached successfully`);
+    res.json({
+      message: `player connected: ${player.id}`,
+      data: {
+        id: player.id,
+        address: player.address,
+        lanAddress: player.lanAddress,
+        serverPort: player.serverPort,
+        gameId: player.gameId,
+        matchFound: player.matchFound,
+        opponent: player.opponent,
+        host: player.host,
+        timeout: this.getTimeUntilDisconnect(player.timeout),
+      },
+    });
+
+    console.log(`player connected: ${player.id}`);
+  }
+
+  info(req, res) {
+    var player = this.players.find((p) => p.id == req.query.playerId);
+    if (player) {
+      res.json({
+        message: `player information: ${player.id}`,
+        data: {
+          id: player.id,
+          address: player.address,
+          lanAddress: player.lanAddress,
+          serverPort: player.serverPort,
+          gameId: player.gameId,
+          matchFound: player.matchFound,
+          opponent: player.opponent,
+          host: player.host,
+          timeout: this.getTimeUntilDisconnect(player.timeout),
+        },
+      });
+    } else {
+      res.json({ message: `player not found: ${req.query.playerId}` });
+    }
+
+    this.resetTimeout(player);
+  }
+
+  disconnect(req, res) {
+    var playerToDisconnect = this.players.indexOf(this.players.find((p) => p.id == req.query.playerId));
+
+    if (playerToDisconnect != -1) {
+      this.removePlayerFromQueue(this.players[playerToDisconnect]);
+      if (this.players.splice(playerToDisconnect)) {
+        console.log(`player disconnected: ${req.query.playerId}`);
+        res.json({ message: `disconnected: ${req.query.playerId}` });
+      } else {
+        res.json({ message: `error disconnecting player: ${req.query.playerId}` });
+      }
+    } else {
+      res.json({ message: `not connected: ${req.query.playerId}` });
+    }
+  }
+
+  ping(req, res) {
+    var player = this.players.find((p) => p.id == req.query.playerId);
+    if (player) {
+      this.resetTimeout(player);
+      res.json({
+        message: `connection ok: ${player.id}`,
+        data: {
+          timeout: this.getTimeUntilDisconnect(player.timeout),
+        },
+      });
+    } else {
+      res.json({ message: `timed out. pls reconnect.` });
+    }
+  }
+
+  getTimeUntilDisconnect(timeout) {
+    return Math.ceil((timeout._idleStart + timeout._idleTimeout - Date.now()) / 1000);
+  }
+
+  resetTimeout(player) {
+    clearTimeout(player.timeout);
+    player.timeout = setTimeout(() => this.timeout(player), 300000);
+  }
+
+  timeout(player) {
+    var playerToDrop = this.players.indexOf(player);
+    this.players.splice(playerToDrop);
+    console.log(`player timed out: ${player.id}`);
+  }
+
+  joinQueue(req, res) {
+    var playerToAdd = this.players.find((p) => p.id == req.query.playerId);
+
+    if (playerToAdd) {
+      playerToAdd.matchFound = false;
+      if (this.addPlayerToQueue(playerToAdd)) {
+        res.json({ message: `added to queue: ${playerToAdd.id}` });
+        console.log(`player added to queue: ${playerToAdd.id}`);
+        this.resetTimeout(playerToAdd);
+      } else {
+        res.json({ message: `already in queue: ${playerToAdd.id}` });
+      }
+    } else {
+      res.json({ message: `not connected: ${req.query.playerId}` });
+    }
+  }
+
+  addPlayerToQueue(player) {
+    var playerToAdd = this.queue.find((p) => p == player);
+
+    if (!playerToAdd) {
+      player.matchFound = false;
+      this.queue.push(player);
+      console.log(this.queue);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  exitQueue(req, res) {
+    var playerToRemove = this.players.find((p) => p.id == req.query.playerId);
+
+    if (playerToRemove) {
+      if (this.removePlayerFromQueue(playerToRemove)) {
+        res.json({ message: `removed from queue: ${playerToRemove.id}` });
+        console.log(`player removed from queue: ${playerToRemove.id}`);
+        this.resetTimeout(playerToRemove);
+      } else {
+        res.json({ message: `not in queue: ${playerToRemove.id}` });
+      }
+    } else {
+      res.json({ message: `not connected: ${req.query.playerId}` });
+    }
+  }
+
+  removePlayerFromQueue(player) {
+    var playerToRemove = this.queue.indexOf(player);
+
+    if (playerToRemove != -1) {
+      this.queue.splice(playerToRemove);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  getQueueStatus(req, res) {
+    var player = this.players.find((p) => p.id == req.query.playerId);
+
+    if (!player) {
+      res.json({
+        message: `not connected`,
+      });
+      return;
+    }
+
+    if (player && player.matchFound && player.opponent) {
+      res.json({
+        message: `match found`,
+        data: player.opponent,
+      });
+
+      this.resetTimeout(player);
+      return;
+    } else if (this.queue.indexOf(player) != -1) {
+      res.json({
+        message: `in queue`,
+        data: {
+          playersInQueue: this.queue.length,
+        },
+      });
+
+      this.resetTimeout(player);
+      return;
+    } else {
+      res.json({
+        message: `not in queue`,
+        data: {
+          playersInQueue: this.queue.length,
+        },
+      });
+    }
   }
 
   startAvailableMatches() {
@@ -96,30 +235,15 @@ export default class QueueController {
         console.log('starting all available matches');
         this.queue.forEach((player, index) => {
           let player1 = player;
-          player1.host = true;
-
-          player.ws.send(
-            JSON.stringify({
-              type: 'start game',
-              data: {
-                networking_mode: 'server',
-              },
-            })
-          );
-
           let player2 = this.queue[index + 1];
-          player2.ws.send(
-            JSON.stringify({
-              type: 'start game',
-              data: {
-                networking_mode: 'client',
-                server_address: `${player.address}`,
-                server_lan_address: `${player.lanAddress}`,
-                server_port: `${player.serverPort}`,
-              },
-            })
-          );
 
+          player1.host = true;
+          player1.matchFound = true;
+          player1.opponent = player2;
+
+          player2.host = false;
+          player2.matchFound = true;
+          player2.opponent = player1;
           this.queue.splice(index, 2);
         });
       } else {
--- server.ts
+++ server.ts
@@ -20,6 +20,60 @@ app.get('/connect', (req, res) => {
   }
 });
 
+app.get('/info', (req, res) => {
+  try {
+    qController.info(req, res);
+  } catch (err) {
+    console.log(`info request failed: ${err}`);
+    res.json(`info request failed: ${err}`);
+  }
+});
+
+app.get('/disconnect', (req, res) => {
+  try {
+    qController.disconnect(req, res);
+  } catch (err) {
+    console.log(`disconnect request failed: ${err}`);
+    res.json(`disconnect request failed: ${err}`);
+  }
+});
+
+app.get('/ping', (req, res) => {
+  try {
+    qController.ping(req, res);
+  } catch (err) {
+    console.log(`ping request failed: ${err}`);
+    res.json(`ping request failed: ${err}`);
+  }
+});
+
+app.get('/joinQueue', (req, res) => {
+  try {
+    qController.joinQueue(req, res);
+  } catch (err) {
+    console.log(`join queue request failed: ${err}`);
+    res.json(`join queue request failed: ${err}`);
+  }
+});
+
+app.get('/exitQueue', (req, res) => {
+  try {
+    qController.exitQueue(req, res);
+  } catch (err) {
+    console.log(`exit queue request failed: ${err}`);
+    res.json(`exit queue request failed: ${err}`);
+  }
+});
+
+app.get('/getQueueStatus', (req, res) => {
+  try {
+    qController.getQueueStatus(req, res);
+  } catch (err) {
+    console.log(`get queue status request failed: ${err}`);
+    res.json(`get queue status request failed: ${err}`);
+  }
+});
+
 app.listen(process.env.PORT || port, () =>
   console.log(`Matchmaking server listening on port ${process.env.PORT || port}`)
 );
